22c56c2d6a18fee60408bcad3e4568bfe7311b0e
BBILV <bukurukijigo@gmail.com>
2021-09-28 16:57:46 +0200

    ma branch

---
+++ Reponse serie 1.js
@@ -0,0 +1,63 @@
+Exercise 1 
+
+Début --->
+Entier A
+TantQue A (<1 ou A >3)
+Afficher --> "Entrez un nombre compris entre 1 et 3") 
+FinTantQue
+afficher merveilleux vous avez gagné
+Fin
+
+
+Exercise 2
+
+Début
+Entier x 
+Pour x=1 jusqu'a x=10{
+afficher X
+x=x+1
+}
+Fin pour
+Fin
+
+Exercise 3
+
+Début
+Chaine X,y
+x="Emma"
+y="nuel"
+affiche la chaine x+y
+Fin
+
+Exercise 4
+
+Début
+Entier age
+Afficher "Entrez votre age svp ?" age
+si age <18 alors "désolé vous êtes mineur"
+sinon age >= 18 alors "Merveileux vous êtes majeure"
+Fin
+
+Exercise 5
+
+Début
+Entier X, y
+Affiche "Entrer un nombre de départ ? : " y
+Pour x=1 jusqu'a x=y{
+y=y+X
+x=x+1
+}fin pour 
+Affiche "Le résultat fait : " y
+Fin
+
+Exercise 4
+
+Entier x = 0 ;
+Entrez votre Age ? x
+Si (x >18) Alors
+Ecrire ("x est Majeure" );
+Sinon
+Ecrire ("x est Mineur" );
+FinSi
+  
+
---
+++ exos.js
@@ -0,0 +1,18 @@
+let p=10;
+
+switch (true) {
+    case p==6 || p==7:
+        
+        console.log('poussin');
+        break;  
+
+    case p==8 || p==9:
+        console.log('Pupille');
+        break;
+        case p==10 || p==11:
+        console.log('Minime');
+        break;
+            
+    default:
+    console.log('cadet');
+}
